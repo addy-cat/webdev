d80d5139cd22fb1a1f367661696d76bc19540ec5
addy_cat <aswantz@pdx.edu>
2022-01-31 17:04:24 -0800

    Updated 05-form

--- hw3/05-form.js
+++ hw3/05-form.js
@@ -1,19 +1,18 @@
 function handleSubmit() {
   // Add your code here
-  const name = document.querySelector('#name');
-  console.log(name);
+  const name = document.querySelector('#name').value;
   const email = document.querySelector('#email').value;
   const comment = document.querySelector('#comment').value;
-  const checkbox = document.querySelector('#checkbox').value;
+  const checkbox = document.querySelector('#checkbox');
 
 
   console.log('========= Form Submission =========');
-  console.log(` Name: ${name}`);
-  console.log(` Email: ${email}`);
-  console.log(` Feedback: ${comment}`);
-  if(checkbox == true){
-    console.log('Yes, I would like to join the newsletter.');
+  console.log(`   Name: ${name}`);
+  console.log(`   Email: ${email}`);
+  console.log(`   Feedback: ${comment}`);
+  if(checkbox.checked == true){
+    console.log('   Yes, I would like to join the newsletter.');
   } else {
-    console.log('No, I would not like to join the newsletter.')
+    console.log('   No, I would not like to join the newsletter.')
   }
 }
